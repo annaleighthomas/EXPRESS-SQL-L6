8a2c5b0cbd0d1b3404540d0d99fbacd047fbd36a
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-05-04 22:07:34 -0400

    passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,102 +15,86 @@ describe('API Routes', () => {
     return client.end();
   });
 
-  const expectedCats = [
+  const expectedAlbums = [
+
     {
       id: expect.any(Number),
-      name: 'Felix',
-      type: 'Tuxedo',
-      url: 'cats/felix.png',
-      year: 1892,
-      lives: 3,
-      isSidekick: false
+      band: 'Quicksand',
+      album: 'Slip',
+      year: 1993,
+      genre: 'Post-Hardcore',
+      img: '',
+      isPlatinum: false
     },
     {
       id: expect.any(Number),
-      name: 'Garfield',
-      type: 'Orange Tabby',
-      url: 'cats/garfield.jpeg',
+      band: 'Minor Threat',
+      album: 'Out of Step',
+      year: 1983,
+      genre: 'Punk Hardcore',
+      img: '',
+      isPlatinum: false
+    },
+    {
+      id: expect.any(Number),
+      band: 'X-Ray Spex',
+      album: 'Gremfree Adolescents',
       year: 1978,
-      lives: 7,
-      isSidekick: false
+      genre: 'Punk Rock',
+      img: '',
+      isPlatinum: false
     },
     {
       id: expect.any(Number),
-      name: 'Duchess',
-      type: 'Angora',
-      url: 'cats/duchess.jpeg',
-      year: 1970,
-      lives: 9,
-      isSidekick: false
+      band: 'Cocks Sparrer',
+      album: 'Shock Troops',
+      year: 1982,
+      genre: 'Punk Rock',
+      img: '',
+      isPlatinum: false
     },
     {
       id: expect.any(Number),
-      name: 'Stimpy',
-      type: 'Manx',
-      url: 'cats/stimpy.jpeg',
-      year: 1990,
-      lives: 1,
-      isSidekick: true
+      band: 'Hole',
+      album: 'Celebrity Skin',
+      year: 1998,
+      genre: 'Alternative Rock',
+      img: '',
+      isPlatinum: true
     },
     {
       id: expect.any(Number),
-      name: 'Sylvester',
-      type: 'Tuxedo',
-      url: 'cats/sylvester.jpeg',
-      year: 1945,
-      lives: 1,
-      isSidekick: true
-    },
-    {
-      id: expect.any(Number),
-      name: 'Tigger',
-      type: 'Orange Tabby',
-      url: 'cats/tigger.jpeg',
-      year: 1928,
-      lives: 8,
-      isSidekick: false
-    },
-    {
-      id: expect.any(Number),
-      name: 'Hello Kitty',
-      type: 'Angora',
-      url: 'cats/hello-kitty.jpeg',
-      year: 1974,
-      lives: 9,
-      isSidekick: false
-    },
-    {
-      id: expect.any(Number),
-      name: 'Hobbs',
-      type: 'Orange Tabby',
-      url: 'cats/hobbs.jpeg',
-      year: 1985,
-      lives: 6,
-      isSidekick: true
+      band: 'Operation Ivy',
+      album: 'Self-titled',
+      year: 1991,
+      genre: 'Punk Rock',
+      img: '',
+      isPlatinum: false
     }
   ];
+  
 
   // If a GET request is made to /api/cats, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data?
-  it('GET /api/cats', async () => {
+  it('GET /api/album', async () => {
     // act - make the request
-    const response = await request.get('/api/cats');
+    const response = await request.get('/api/albums');
 
     // was response OK (200)?
     expect(response.status).toBe(200);
 
     // did it return the data we expected?
-    expect(response.body).toEqual(expectedCats);
+    expect(response.body).toEqual(expectedAlbums);
 
   });
 
   // If a GET request is made to /api/cats/:id, does:
   // 1) the server respond with status of 200
   // 2) the body match the expected API data for the cat with that id?
-  test('GET /api/cats/:id', async () => {
-    const response = await request.get('/api/cats/2');
+  test('GET /api/albums/:id', async () => {
+    const response = await request.get('/api/albums/2');
     expect(response.status).toBe(200);
-    expect(response.body).toEqual(expectedCats[1]);
+    expect(response.body).toEqual(expectedAlbums[1]);
   });
 });
