56bfc43a8044f78159c71b67709a98e0134d9c41
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-05-06 18:05:46 -0400

    giex eslint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -21,63 +21,63 @@ describe('API Routes', () => {
 
  
 
-    // const expectedAlbums = [
+    const expectedAlbums = [
 
-    //   {
-    //     id: expect.any(Number),
-    //     band: 'Quicksand',
-    //     album: 'Slip',
-    //     year: 1993,
-    //     genre: 'Post-Hardcore',
-    //     img: '',
-    //     isPlatinum: false
-    //   },
-    //   {
-    //     id: expect.any(Number),
-    //     band: 'Minor Threat',
-    //     album: 'Out of Step',
-    //     year: 1983,
-    //     genre: 'Punk Hardcore',
-    //     img: '',
-    //     isPlatinum: false
-    //   },
-    //   {
-    //     id: expect.any(Number),
-    //     band: 'X-Ray Spex',
-    //     album: 'Gremfree Adolescents',
-    //     year: 1978,
-    //     genre: 'Punk Rock',
-    //     img: '',
-    //     isPlatinum: false
-    //   },
-    //   {
-    //     id: expect.any(Number),
-    //     band: 'Cocks Sparrer',
-    //     album: 'Shock Troops',
-    //     year: 1982,
-    //     genre: 'Punk Rock',
-    //     img: '',
-    //     isPlatinum: false
-    //   },
-    //   {
-    //     id: expect.any(Number),
-    //     band: 'Hole',
-    //     album: 'Celebrity Skin',
-    //     year: 1998,
-    //     genre: 'Alternative Rock',
-    //     img: '',
-    //     isPlatinum: true
-    //   },
-    //   {
-    //     id: expect.any(Number),
-    //     band: 'Operation Ivy',
-    //     album: 'Self-titled',
-    //     year: 1991,
-    //     genre: 'Punk Rock',
-    //     img: '',
-    //     isPlatinum: false
-    //   }
-    // ];
+      {
+        id: expect.any(Number),
+        band: 'Quicksand',
+        album: 'Slip',
+        year: 1993,
+        genre: 'Post-Hardcore',
+        img: '',
+        isPlatinum: false
+      },
+      {
+        id: expect.any(Number),
+        band: 'Minor Threat',
+        album: 'Out of Step',
+        year: 1983,
+        genre: 'Punk Hardcore',
+        img: '',
+        isPlatinum: false
+      },
+      {
+        id: expect.any(Number),
+        band: 'X-Ray Spex',
+        album: 'Gremfree Adolescents',
+        year: 1978,
+        genre: 'Punk Rock',
+        img: '',
+        isPlatinum: false
+      },
+      {
+        id: expect.any(Number),
+        band: 'Cocks Sparrer',
+        album: 'Shock Troops',
+        year: 1982,
+        genre: 'Punk Rock',
+        img: '',
+        isPlatinum: false
+      },
+      {
+        id: expect.any(Number),
+        band: 'Hole',
+        album: 'Celebrity Skin',
+        year: 1998,
+        genre: 'Alternative Rock',
+        img: '',
+        isPlatinum: true
+      },
+      {
+        id: expect.any(Number),
+        band: 'Operation Ivy',
+        album: 'Self-titled',
+        year: 1991,
+        genre: 'Punk Rock',
+        img: '',
+        isPlatinum: false
+      }
+    ];
 
     let tearItUp = {
       id: expect.any(Number),
